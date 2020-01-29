1e4f888a294ece84401593a1db87db0000691cc8
sgour30 <samarthgour123@gmail.com>
2020-01-29 13:42:59 +0530

    update

---
+++ day6/lowhighchar.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{     
+	int n,i,j;
+	char arr[10],max,min;
+	for(i=0;i<10;i++)
+	{
+		scanf("%c ",&arr[i]);
+	}
+        max=arr[0];
+	for(i=0;i<10;i++)
+	{
+		if(arr[i]>max)
+		{
+			max=arr[i];
+		}
+	}
+	printf("max character is %c" ,max);
+	min=arr[0];
+        for(i=0;i<10;i++)
+        {
+                if(arr[i]<min)
+                {
+                        min=arr[i];
+                }
+        }
+        printf("\nmin character is %c" ,min);
+
+        	
+
+}       	
+	
+	
+	
