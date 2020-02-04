45719e3333d0504e70338c0aa7cf63f52f0b566d
sgour30 <samarthgour123@gmail.com>
2020-02-04 14:23:18 +0530

    update

---
+++ practice/2darray.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+  printf("Enter the number of rows and columns");
+  int row, col;
+  scanf("%d %d",&row,&col);
+  int **ptr =(int**)calloc(row,sizeof(int*));
+  for(int i=0;i<row;i++)
+  {
+           
+	  ptr[i]=(int*)calloc(col,sizeof(int));
+
+   }
+   for(int i=0;i<row;i++)
+  {
+	  for(int j=0;j<col;j++)
+	  {
+            scanf("%d",&ptr[i][j]);
+	  }
+  }
+  for(int i=0;i<row;i++)
+  {
+	  for(int j=0;j<col;j++)
+	  {
+		  printf("%d",ptr[i][j]);
+	  }
+
+  }
+}
