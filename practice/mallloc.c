9c298eb6f234720c26072a7cbe21e88f30d1b344
sgour30 <samarthgour123@gmail.com>
2020-02-04 12:41:12 +0530

    practice

---
+++ practice/mallloc.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+# define NULL "error"
+int main()
+{
+/*	int a=10;
+	int *ptr= &a;
+//	char **t=ptr;
+//	printf("%d",&ptr);
+	void **vptr;
+	vptr=&ptr;
+	printf("%d",**(int ***)vptr);
+*/
+
+
+/*      char a=33 ,b=12;
+     // char *p=&a ,*q=&b;
+      printf("%c",63);
+      return 0;
+*/
+
+
+	
+   
+
+
+ 	char *ptr = NULL;
+ 	printf("%s",ptr);
+ 	return 0;
+
+
+
+
+
+	
+}
---
+++ practice/malloc.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+  printf("Enter size of array");
+  int n;
+  scanf("%d",&n);
+  int*ptr =(int*)malloc(n*sizeof(int));
+  printf("Enter Array");
+  for(int i=0;i<n;i++)
+  {
+	  scanf("%d",&ptr[i]);
+
+   }
+  for(int i=0;i<n;i++)
+  {
+   printf("%d",ptr[i]);
+  }
+
+
+  	  
+}
+
