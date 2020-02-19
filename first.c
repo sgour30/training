79eba4f95e4f93ab928af36474b19cf10db83e3e
sgour30 <samarthgour123@gmail.com>
2020-02-19 10:53:39 +0530

    update

--- first.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	printf("Samarth");
-	return 0;
-}
-
--- try1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-	char name[]="\0HiHello";
-	printf("%li%li\n",strlen(name),sizeof(name));
-	return 0;
-}
